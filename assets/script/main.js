ebfefc15f4968196e29d4abc3dd414f9fe458da6
xhonatankonomi <xhoni8962@gmail.com>
2025-10-07 23:13:14 +0200

    fix: Correctly print FizzBuzz for multiples of 15
    This commit fixes an issue where multiples of 15 were incorrectly
    printed as Fizz. The logic is updated to print "FizzBuzz" when
    the number is divisible by both 3 and 5.

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,13 +15,13 @@
 
  for (let i = 1; i <= 100; i++) {
     console.log(i)
-    if (i % 3 == 0) {
-        console.log("Fizz")
+    if (i % 15 == 0) {
+        console.log("FizzBuzz")
     }
     else if (i % 5 == 0) {
         console.log("Buzz")
     }
-    else if (i % 15 == 0) {
-        console.log("FizzBuzz")
+    else if (i % 3 == 0) {
+        console.log("Fizz")
     }
  }
