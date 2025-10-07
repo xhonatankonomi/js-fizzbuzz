88b75b97f81ee3d256b2779df85bc0637c56a07b
xhonatankonomi <xhoni8962@gmail.com>
2025-10-07 23:07:30 +0200

    feat: Implement FizzBuzz logic in main.js
    This commit introduces the FizzBuzz logic to the main.js file.
    It prints "Fizz" for multiples of 3 and "Buzz" for multiples of 5.

--- assets/script/main.js
+++ assets/script/main.js
@@ -16,7 +16,9 @@
  for (let i = 1; i <= 100; i++) {
     console.log(i)
     if (i % 3 == 0) {
-        console.log(Fizz)
+        console.log("Fizz")
     }
-    else if (i % 5 == 0)
+    else if (i % 5 == 0) {
+        console.log("Buzz")
+    }
  }
